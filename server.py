42d465bffc2063d67b7bd6936db62554e3707536
Vlad Dmitrievich <me@2tunnels.com>
2019-12-11 17:13:22 +0200

    return method, headers, query and post data in response

--- server.py
+++ server.py
@@ -2,7 +2,12 @@ from aiohttp import web
 
 
 async def handler(request: web.Request) -> web.Response:
-    return web.Response(text='Hello, world!')
+    return web.json_response({
+        'method': request.method,
+        'headers': dict(request.headers),
+        'query': dict(request.query),
+        'post': dict(await request.post()),
+    })
 
 
 app = web.Application()
